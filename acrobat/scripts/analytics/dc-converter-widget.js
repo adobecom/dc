b8fc344a020b2c59fde613a8241a0eacaf8fb94b
Blaine Gunn <gunn@adobe.com>
2023-01-07 16:39:28 -0700

    MWPW-123571

---
+++ acrobat/scripts/analytics/dc-converter-widget.js
@@ -0,0 +1,39 @@
+export default function init(element) {
+  console.log(element);
+  console.log('ALLOY');
+
+  // Figure out how to setp LH
+  const lh = 'SOME DEFAULT VALUE';
+
+  const event = {
+    // always trigger the event using navigator.sendBeacon
+    documentUnloading: true,
+    data: {
+      eventType: 'web.webinteraction.linkClicks',
+      web: {
+        webInteraction: {
+          linkClicks: { value: 1 },
+          type: 'other',
+          name: lh,
+          // URL: '',
+          // region: '',
+        },
+      },
+      _adobe_corpnew: {
+        digitalData: {
+          primaryEvent: {
+            eventInfo: {
+              interaction: {
+                click: lh,
+                iclick: 'true',
+              },
+              eventName: lh,
+            },
+          },
+        },
+      },
+    },
+  };
+
+  window._satellite.track('event', event);
+}
