557d7455623ce3df282a3744fe5724dca1ad1535
Blaine Gunn <gunn@adobe.com>
2023-01-26 14:58:20 -0700

    MWPW-124169

--- acrobat/scripts/scripts.js
+++ acrobat/scripts/scripts.js
@@ -57,7 +57,7 @@ const CONFIG = {
     }
   },
   codeRoot: '/acrobat',
-  contentRoot: '/acrobat',
+  contentRoot: '/dc-shared',
   imsClientId: 'acrobatmilo',
   local: { edgeConfigId: 'da46a629-be9b-40e5-8843-4b1ac848745c' },
   stage: { edgeConfigId: 'da46a629-be9b-40e5-8843-4b1ac848745c' },
