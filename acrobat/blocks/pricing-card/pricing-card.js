9cd8a4cb7339eef39e96b1ad15b3d02a31e7adbc
Sonja Popovic <sonja.popovic@lilly021.com>
2023-04-03 09:44:10 +0200

    MWPW-122642-refactor; fix to work with more than one card on page; add promotion text conditionally

--- acrobat/blocks/pricing-card/pricing-card.js
+++ acrobat/blocks/pricing-card/pricing-card.js
@@ -3,75 +3,73 @@ import {createTag} from "../../scripts/miloUtils.js";
 const init = (block) => {
   createTag.then((createTag) => {
     const properties = getProperties(block);
-    const cardContainer = createTag('div', { class: 'content-container' });
-    const cardContent = createTag('div', { class: 'card-box' }, cardContainer);
-    const promotionText = createTag('div', {class: 'promotion-text'});
-    promotionText.innerText = properties['promotionText'];
-    const title = createTag('div', {class: 'title'});
-    title.innerText = properties['title'];
-    const price = createTag('div', {class: 'price'});
-    const disclaimer = createTag('div', {class: 'disclaimer'});
-    const radioGroup = createTag('div', {class: 'radio-group'});
-
-    for (let i = 1; i <= properties['countOptions']; i++) {
-
-
-      const currentOption = `option${i}`;
-      const radioInput = createTag('input', {
-        type: 'radio',
-        name: 'selectedOption',
-        value: properties[currentOption],
-        id: currentOption + '-' + properties[currentOption],
-        option: currentOption
-      });
-      if (properties['initialOption'] === currentOption) {
-        radioInput.setAttribute('checked', true);
-      }
-      radioInput.addEventListener('change', (e) => toggleContent(e.target.getAttribute('option'),block));
-
-
-
-      const label = createTag('label', {for: currentOption + '-' + properties[currentOption]});
-      label.innerText = properties[currentOption];
-
-      const option = createTag('div', {class: 'radio-item'});
-      option.append(radioInput);
-      option.append(label);
-
-      radioGroup.append(option);
-
-
-    }
-
-    const divider = createTag('div', {class: 'divider'});
-    const footer = createTag('div', {class: 'card-footer'});
-    const footerSecureTransaction =  createTag('div', {class: 'footer-secure-transaction'});
-    const footerImg = createTag('img', {class: 'icon', src: '/acrobat/blocks/pricing-card/lock-icon-grey-40x50.png'});
-    const secureTransactionText = createTag('span', {class: 'text'}, 'Secure transaction');
-    footerSecureTransaction.append(footerImg);
-    footerSecureTransaction.append(secureTransactionText);
-    const footerContent = createTag('div', {class: 'footer-content'});
-    footer.append(footerSecureTransaction);
-    footer.append(footerContent);
-
-    cardContent.insertBefore(promotionText, cardContainer);
-    cardContainer.append(title);
-    cardContainer.append(price);
-    cardContainer.append(disclaimer);
-    cardContainer.append(radioGroup);
-    cardContainer.append(divider);
-    cardContainer.append(footer);
-    block.append(cardContent);
-
-
+    preparePricingCardDOM(block, properties, createTag);
     collectContent(block, properties);
     toggleContent(properties['initialOption'], block);
-
   });
 };
-const getProperties = (table) => {
+const preparePricingCardDOM = (block, properties, createTag) => {
+  const cardContainer = createTag('div', { class: 'content-container' });
+  const cardContent = createTag('div', { class: 'card-box' }, cardContainer);
+  if (properties['promotionText']) {
+    const promotionText = createTag('div', {class: 'promotion-text'}, properties['promotionText']);
+    cardContent.insertBefore(promotionText, cardContainer);
+    cardContent.classList.add('promotion-active');
+  }
+  const title = createTag('div', {class: 'title'}, properties['title']);
+  const price = createTag('div', {class: 'price'});
+  const disclaimer = createTag('div', {class: 'disclaimer'});
+  const radioGroup = createRadioGroup(properties, createTag, block);
+  const divider = createTag('div', {class: 'divider'});
+  const footer = createFooter(properties, createTag);
+  cardContainer.append(title);
+  cardContainer.append(price);
+  cardContainer.append(disclaimer);
+  cardContainer.append(radioGroup);
+  cardContainer.append(divider);
+  cardContainer.append(footer);
+  block.append(cardContent);
+}
+const createRadioGroup = (properties, createTag, block) => {
+  const radioGroup = createTag('div', {class: 'radio-group'});
+  for (let i = 1; i <= properties['countOptions']; i++) {
+    const currentOption = `option${i}`;
+    const radioInput = createTag('input', {
+      type: 'radio',
+      name: 'selectedOption' + properties['title'],
+      value: properties[currentOption],
+      id: currentOption + '-' + properties[currentOption] + '-' + properties['title'],
+      option: currentOption
+    });
+    if (properties['initialOption'] === currentOption) {
+      radioInput.setAttribute('checked', true);
+    }
+    radioInput.addEventListener('change', (e) => toggleContent(e.target.getAttribute('option'), block));
+    const label = createTag('label', {for: currentOption + '-' + properties[currentOption] + '-' + properties['title'],});
+    label.innerText = properties[currentOption];
+
+    const radioItem = createTag('div', {class: 'radio-item'});
+    radioItem.append(radioInput);
+    radioItem.append(label);
+    radioGroup.append(radioItem);
+  }
+  return radioGroup;
+}
+const createFooter = (properties, createTag) => {
+  const footer = createTag('div', {class: 'card-footer'});
+  const footerSecureTransaction =  createTag('div', {class: 'footer-secure-transaction'});
+  const footerImg = createTag('img', {class: 'icon', src: '/acrobat/blocks/pricing-card/lock-icon-grey-40x50.png'});
+  const secureTransactionText = createTag('span', {class: 'text'}, 'Secure transaction');
+  footerSecureTransaction.append(footerImg);
+  footerSecureTransaction.append(secureTransactionText);
+  const footerContent = createTag('div', {class: 'footer-content'});
+  footer.append(footerSecureTransaction);
+  footer.append(footerContent);
+  return footer;
+}
+const getProperties = (block) => {
   const properties = {};
-  const tableRows = table.querySelectorAll(':scope > div');
+  const tableRows = block.querySelectorAll(':scope > div');
   let countOptions = 0;
   tableRows.forEach((row) => {
     if (row.children[0] && row.children[0].innerText) {
@@ -82,26 +80,9 @@ const getProperties = (table) => {
   properties.countOptions = countOptions;
   return properties;
 };
-const getContent = (selectedOption) =>  {
-  const optionContent = document.querySelectorAll(`.section.${selectedOption}`);
-let price;
-optionContent.forEach((content) => {
-    const optionProperties = getProperties(content.querySelector('.section-metadata'));
-    if (optionProperties['place'] === 'price') {
-      price =  content;
-    };
-  });
-return price;
-};
-const radioChangeHandler = (radio, card) => {
-const priceOption = getPrice(radio.target.getAttribute('option'));
-   card.querySelector('.price').append(priceOption);
-};
-
 const collectContent = (cardNode, cardProperties) => {
   const prices = [];
   const disclaimers = [];
-  const selectedOption = cardProperties['initialOption'];
   // todo implement footers (ctas, qty selectors)
   const footers = [];
   for (let i = 1; i <= cardProperties['countOptions']; i++) {
@@ -148,3 +129,13 @@ export default init;
 // todo 1: promotion text:
 // todo 1: make border collor customizable (orange or black depending on promotion existence)
 // todo 1: text can have more lines and different style for each line
+
+// todo 2: toggle content:
+// todo 2: what will be content for price/disclaimer/footer and what parts we should include in pricing cards, how it will be created
+
+// todo 3:
+// make sure that we hide/show (manipulate) with dom only inside current pricing card
+// test with more than one pricing card on the page
+
+// todo 4:
+// should we create a container element where we can put all pricing cards inside it (maybe create card container that will pick all pricing cards from page)
