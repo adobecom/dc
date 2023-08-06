336aa634b2e89113cc8fb865e8972b083ca71fae
Adam Peller <peller@adobe.com>
2023-08-05 22:30:26 -0400

    prefetch dc-core

--- acrobat/scripts/scripts.js
+++ acrobat/scripts/scripts.js
@@ -210,10 +210,11 @@ const { ietf } = getLocale(locales);
       widgetBlock.removeAttribute('class');
       widgetBlock.id = 'dc-converter-widget'; 
       const DC_WIDGET_VERSION = document.querySelector('meta[name="dc-widget-version"]')?.getAttribute('content');
-      const [,DC_GENERATE_CACHE_VERSION] = DC_WIDGET_VERSION.split('_');
+      const [DC_CORE_VERSION, DC_GENERATE_CACHE_VERSION] = DC_WIDGET_VERSION.split('_');
       const dcUrls = [
         `https://acrobat.adobe.com/dc-hosted/${DC_WIDGET_VERSION}/dc-app-launcher.js`,
-        `https://acrobat.adobe.com/dc-generate-cache/dc-hosted-${DC_GENERATE_CACHE_VERSION}/${window.location.pathname.split('/').pop().split('.')[0]}-${ietf.toLowerCase()}.html`
+        `https://acrobat.adobe.com/dc-generate-cache/dc-hosted-${DC_GENERATE_CACHE_VERSION}/${window.location.pathname.split('/').pop().split('.')[0]}-${ietf.toLowerCase()}.html`,
+        `https://acrobat.adobe.com/dc-core/${DC_CORE_VERSION}/dc-core.js`
       ];
 
       dcUrls.forEach( url => {
