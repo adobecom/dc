e0ae33f892a0fac3b63f209cc59ed7f12973dfe6
Adam Peller <peller@adobe.com>
2023-08-29 22:13:22 -0400

    don't need to use page path anymore

--- acrobat/scripts/scripts.js
+++ acrobat/scripts/scripts.js
@@ -242,7 +242,6 @@ const { ietf } = getLocale(locales);
 
   // Fast track the widget
   const widgetBlock = document.querySelector('[class*="dc-converter-widget"]');
-  const pageName = window.location.pathname.split('/').pop().split('.')[0];
   if (widgetBlock) {
     const verb = widgetBlock.children[0].children[0]?.innerText?.trim();
     const blockName = widgetBlock.classList.value;
@@ -255,7 +254,7 @@ const { ietf } = getLocale(locales);
     ];
 
     // Experiment, don't prefetch app-launcher as it competes with fast path
-    if (pageName !== 'compress-pdf') {
+    if (verb !== 'compress-pdf') {
       dcUrls.push(`https://acrobat.adobe.com/dc-hosted/${DC_WIDGET_VERSION}/dc-app-launcher.js`);
     }
 
