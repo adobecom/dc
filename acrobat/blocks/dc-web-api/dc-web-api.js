94a9c70a6bcc090c07e3d65a57a843cb42323582
Blaine Gunn <gunn@adobe.com>
2023-09-18 12:32:54 -0600

    soft touch

---
+++ acrobat/blocks/dc-web-api/dc-web-api.js
@@ -0,0 +1,83 @@
+import {createTag} from "../../scripts/miloUtils.js";
+const {loadScript} = await import(`https://main--milo--adobecom.hlx.page/libs/utils/utils.js`);
+
+const handleDragOver = (e) => {
+  e.preventDefault();
+}
+
+const handleDrop = (e) => {
+  // let datas;
+  e.preventDefault();
+  console.log(e.dataTransfer.files);
+  if (e.dataTransfer.items) {
+    [...e.dataTransfer.items].forEach((item, i) => {
+      if (item.kind === "file") {
+        const file = item.getAsFile();
+        console.log(`… file[${i}].name = ${file.name}`);
+      }
+    });
+  } else {
+    [...e.dataTransfer.files].forEach((file, i) => {
+      console.log(`… file[${i}].name = ${file.name}`);
+    });
+  }
+
+}
+
+
+
+export default function init(element) {
+  console.log(element.querySelectorAll(':scope > div'));
+        //Create Fake Widget
+        createTag.then((tag) => {
+          const content = element.querySelectorAll(':scope > div');
+
+          Array.from(content).forEach( (con) =>{
+            con.classList.add('hide')
+          })
+
+          const wrapper = tag('div', {id: 'CID', class: `fsw widget-wrapper ` });
+          const heading = tag('h1', { class: 'widget-heading' }, `${content[1].textContent}`);
+          const dropZone = tag('div', { id: 'dZone', class: 'widget-center' });
+          const copy = tag('p', { class: 'widget-copy' }, `${content[2].textContent}`);
+          const button = tag('input', { type: 'file', id: 'file-upload', class: 'hide' }, `${content[3].textContent}`);
+          const buttonLabel = tag('label', { for: 'file-upload', class: 'widget-button' }, `${content[3].textContent}`);
+          const legal = tag('p', { class: 'widget-legal' }, `${content[4].textContent}`);
+          const icon = tag('p',{ class: 'widget-sub' } , 'Adobe Acrobat');
+          wrapper.append(icon);
+          wrapper.append(heading);
+          wrapper.append(dropZone)
+          dropZone.append(copy);
+          dropZone.append(button);
+          dropZone.append(buttonLabel);
+          wrapper.append(legal);
+          element.append(wrapper);
+
+          dropZone.addEventListener('dragover', (file) => {
+            handleDragOver(file);
+          })
+
+          dropZone.addEventListener('drop', (file) => {
+            handleDrop(file);
+            //make call to dc web and pass over file 
+            //https://api-ninjas.com/signin
+
+            const dcWidgetScript = tag('script', {
+              id: 'adobe_dc_sdk_launcher',
+              src: 'https://stage.acrobat.adobe.com/dc-hosted/3.10.0_2.16.2/dc-app-launcher.js',
+              'data-dropzone_id': 'CID',
+              'data-locale': 'us-en',
+              'data-server_env': 'stage',
+              'data-verb': 'word-to-pdf',
+              'data-load_typekit': 'false',
+              'data-load_imslib': 'false',
+              'data-enable_unload_prompt': 'true',
+            });
+            element.append(dcWidgetScript);
+            // loadScript('https://stage.acrobat.adobe.com/dc-hosted/3.10.0_2.16.2/dc-app-launcher.js');
+
+
+          })
+        })
+
+}
---
+++ acrobat/blocks/dc-web-api/verbMap.js
@@ -0,0 +1,53 @@
+const verbMap = {
+  typeMap: {
+    'word-to-pdf': 'create_pdf',
+    'ppt-to-pdf': 'create_pdf',
+    'jpg-to-pdf': 'create_pdf',
+    'png-to-pdf': 'create_pdf',
+    'excel-to-pdf': 'create_pdf',
+    createpdf: 'create_pdf',
+    'compress-pdf': 'compress_pdf',
+    'pdf-to-excel': 'export_pdf',
+    'pdf-to-image': 'export_pdf',
+    'pdf-to-ppt': 'export_pdf',
+    'pdf-to-word': 'export_pdf',
+    'combine-pdf': 'combine_pdf',
+    'protect-pdf': 'protect_pdf',
+    'delete-pages': 'organize_pdf',
+    'reorder-pages': 'organize_pdf',
+    'rotate-pages': 'organize_pdf',
+    'split-pdf': 'organize_pdf',
+    'insert-pdf': 'organize_pdf',
+    'extract-pages': 'organize_pdf',
+    L1: 'organize_pdf',
+  },
+  upsellMap: {
+    'word-to-pdf': 'createPDF',
+    'ppt-to-pdf': 'createPDF',
+    'jpg-to-pdf': 'createPDF',
+    'png-to-pdf': 'createPDF',
+    'excel-to-pdf': 'createPDF',
+    createpdf: 'createPDF',
+    'compress-pdf': 'compressPDF',
+    'pdf-to-excel': 'exportPDF',
+    'pdf-to-image': 'exportPDF',
+    'pdf-to-ppt': 'exportPDF',
+    'pdf-to-word': 'exportPDF',
+    'combine-pdf': 'mergePDF',
+    'protect-pdf': 'passwordProtectPDF',
+    'delete-pages': 'l1Verbs',
+    'reorder-pages': 'l1Verbs',
+    'rotate-pages': 'l1Verbs',
+    'split-pdf': 'l1Verbs',
+    'insert-pdf': 'l1Verbs',
+    'extract-pages': 'l1Verbs',
+  },
+};
+
+export default verbMap;
+
+// L1 Verbs with no real quota
+// fillsign - upload
+// sendforsignature  - upload
+// add-comment (edit) - upload
+// crop-pages - upload
