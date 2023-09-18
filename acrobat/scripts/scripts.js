c36b0f4b98e7952bc58dc2980f112a899f78dd12
Blaine Gunn <gunn@adobe.com>
2023-09-18 13:01:48 -0600

    soft touch

--- acrobat/scripts/scripts.js
+++ acrobat/scripts/scripts.js
@@ -260,31 +260,31 @@ const { ietf } = getLocale(locales);
 
 (async function loadPage() {
   // Fast track the widget
-  const widgetBlock = document.querySelector('[class*="dc-converter-widget"]');
-  if (widgetBlock) {
-    const verb = widgetBlock.children[0].children[0]?.innerText?.trim();
-    const blockName = widgetBlock.classList.value;
-    widgetBlock.removeAttribute('class');
-    widgetBlock.id = 'dc-converter-widget';
-    const DC_WIDGET_VERSION = document.querySelector('meta[name="dc-widget-version"]')?.getAttribute('content');
-    const DC_GENERATE_CACHE_VERSION = document.querySelector('meta[name="dc-generate-cache-version"]')?.getAttribute('content');
-    const dcUrls = [
-      `https://www.adobe.com/dc/dc-generate-cache/dc-hosted-${DC_GENERATE_CACHE_VERSION}/${verb}-${ietf.toLowerCase()}.html`,
-    ];
+  // const widgetBlock = document.querySelector('[class*="dc-converter-widget"]');
+  // if (widgetBlock) {
+  //   const verb = widgetBlock.children[0].children[0]?.innerText?.trim();
+  //   const blockName = widgetBlock.classList.value;
+  //   widgetBlock.removeAttribute('class');
+  //   widgetBlock.id = 'dc-converter-widget';
+  //   const DC_WIDGET_VERSION = document.querySelector('meta[name="dc-widget-version"]')?.getAttribute('content');
+  //   const DC_GENERATE_CACHE_VERSION = document.querySelector('meta[name="dc-generate-cache-version"]')?.getAttribute('content');
+  //   const dcUrls = [
+  //     `https://www.adobe.com/dc/dc-generate-cache/dc-hosted-${DC_GENERATE_CACHE_VERSION}/${verb}-${ietf.toLowerCase()}.html`,
+  //   ];
 
-    dcUrls.forEach( url => {
-      const link = document.createElement('link');
-      link.setAttribute('rel', 'prefetch');
-      if(url.split('.').pop() === 'html') {link.setAttribute('as', 'fetch');}
-      if(url.split('.').pop() === 'js') {link.setAttribute('as', 'script');}
-      link.setAttribute('href', url);
-      link.setAttribute('crossorigin', '');
-      document.head.appendChild(link);
-    })
+  //   dcUrls.forEach( url => {
+  //     const link = document.createElement('link');
+  //     link.setAttribute('rel', 'prefetch');
+  //     if(url.split('.').pop() === 'html') {link.setAttribute('as', 'fetch');}
+  //     if(url.split('.').pop() === 'js') {link.setAttribute('as', 'script');}
+  //     link.setAttribute('href', url);
+  //     link.setAttribute('crossorigin', '');
+  //     document.head.appendChild(link);
+  //   })
 
-    const { default: dcConverter } = await import(`../blocks/${blockName}/${blockName}.js`);
-    await dcConverter(widgetBlock);
-  }
+  //   const { default: dcConverter } = await import(`../blocks/${blockName}/${blockName}.js`);
+  //   await dcConverter(widgetBlock);
+  // }
 
   // Setup CSP
   (async () => {
