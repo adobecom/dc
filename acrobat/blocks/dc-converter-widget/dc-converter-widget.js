7da7bbf0d489c1675699feaaa471912a1f31a2f4
Purushotham Doreswamachary <doreswam@purushothamsmbp.corp.adobe.com>
2023-09-19 13:44:28 -0700

    MWPW-136317 Dropzone enabled on select verbs, only on mobile view

--- acrobat/blocks/dc-converter-widget/dc-converter-widget.js
+++ acrobat/blocks/dc-converter-widget/dc-converter-widget.js
@@ -318,7 +318,7 @@ export default async function init(element) {
     })
   }
 
-  if (VERB === 'compress-pdf' || VERB === 'reorder-pages' || preRenderDropZone) {
+  if (window.browser?.isMobile && (VERB === 'fillsign' || VERB === 'sendforsignature' || VERB === 'add-comment' || VERB === 'crop-pages' || VERB === 'number-pages')) {
     const verbFromURL = window.location.pathname.split('/').pop().split('.')[0];
     const response = await fetch(DC_GENERATE_CACHE_URL || `${DC_DOMAIN}/dc-generate-cache/dc-hosted-${DC_GENERATE_CACHE_VERSION}/${VERB}-${pageLang}.html`);
     switch (response.status) {
