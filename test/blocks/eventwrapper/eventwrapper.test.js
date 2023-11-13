6e4a10ab6337fde217e87c8409c1ba9397b44e16
Dragana Trajkovic <dragana.trajkovic@lilly021.com>
2023-11-13 16:18:42 +0100

    MWPW-138678 - Improve unit test coverage

--- test/blocks/eventwrapper/eventwrapper.test.js
+++ test/blocks/eventwrapper/eventwrapper.test.js
@@ -5,11 +5,12 @@ import { delay } from '../../helpers/waitfor.js';
 
 const head = await readFile({ path: './mocks/head.html' });
 const body = await readFile({ path: './mocks/body.html' });
+const bodyRotatePdf = await readFile({ path: './mocks/body-rotate-pdf.html' });
 const { default: init } = await import(
   '../../../acrobat/blocks/eventwrapper/eventwrapper'
 );
 
-describe('eventwrapper block', () => {
+describe('eventwrapper block on Chrome', () => {
   let browserName = 'Chrome';
   let chromeRuntimeSendMessage = false;
 
@@ -113,27 +114,6 @@ describe('eventwrapper block', () => {
     expect(event).to.eql('Get the extension-1|already-closed-viewer-extension|Chrome-extension');
   });
 
-  it('shows the ext modal on MS Edge when conversion complete and preview displayed', async function () {
-    browserName = 'Microsoft Edge';
-    document.head.innerHTML = head;
-    document.body.innerHTML = body;
-    window.dc_hosted.listeners = [];
-    localStorage.removeItem('fricBrowExt');  
-    window.modalDisplayed = false;
-    window.chrome = {
-      runtime: {},
-    };
-    const blocks = document.body.querySelectorAll('.eventwrapper');
-    blocks.forEach((x) => init(x));
-    window.dispatchEvent(new CustomEvent('DC_Hosted:Ready'));
-    window.dc_hosted.dispatchEvent('conversion-complete', {});
-    expect(window.modalDisplayed).to.be.true;
-
-    window.modalDisplayed = false;
-    window.dc_hosted.dispatchEvent('preview-displayed', {});
-    expect(window.modalDisplayed).to.be.true;
-  });
-
   it('handles processing-start', async function () {
     window.dc_hosted.dispatchEvent('processing-start', {});
     expect(document.body.dataset.currentEvent).to.eql('start');
@@ -154,14 +134,19 @@ describe('eventwrapper block', () => {
     expect(document.body.dataset.currentEvent).to.eql('cancel');
   });
 
+  it('handles processing-complete', async function () {
+    window.dc_hosted.dispatchEvent('processing-complete', {});
+    expect(document.body.dataset.currentEvent).to.eql('complete');
+  });
+
   it('handles try-another-file-start', async function () {
     window.dc_hosted.dispatchEvent('try-another-file-start', {});
-    expect(window.modalDisplayed).to.be.false;  
+    expect(window.modalDisplayed).to.be.false;
   });
 
   it('handles preview-generating', async function () {
     window.dc_hosted.dispatchEvent('preview-generating', {});
-    expect(document.body.dataset.currentEvent).to.eql('preview');   
+    expect(document.body.dataset.currentEvent).to.eql('preview');
   });
 
   it('handles preview-displayed', async function () {
@@ -177,5 +162,145 @@ describe('eventwrapper block', () => {
   it('handles download-start', async function () {
     window.dc_hosted.dispatchEvent('download-start', {});
     expect(document.body.dataset.currentEvent).to.eql('download');
+  });
+
+  it('handles default case', async function () {
+    //download is last currentEvent before unknown-event
+    window.dc_hosted.dispatchEvent('unknown-event', {});
+    expect(document.body.dataset.currentEvent).to.eql('download');
+  });
+
+  it('hide content for processing-start on rotate-pdf', async function () {
+    document.head.innerHTML = head;
+    document.body.innerHTML = bodyRotatePdf;
+    const blocks = document.body.querySelectorAll('.eventwrapper');
+    blocks.forEach((x) => init(x));
+    window.dispatchEvent(new CustomEvent('DC_Hosted:Ready'));
+    window.dc_hosted.dispatchEvent('processing-start', {});
+    expect(document.body.dataset.currentEvent).to.eql('start');
+    expect(document.body.classList.contains('l2-state')).to.be.equal(true);
+    expect(document.body.classList.contains('hide-content')).to.be.equal(true);
+    const widget = document.querySelector('[data-section="widget"]');
+    expect(widget.classList.contains('widget-default-height')).to.be.equal(false);
+    const contentSections = document.querySelectorAll('.section:not([data-section=\'widget\'])');
+    contentSections.forEach((section) => expect(section.classList.contains('hide')).to.be.equal(true));
+  });
+  it('show content for preview-generating on rotate-pdf', async function () {
+    document.head.innerHTML = head;
+    document.body.innerHTML = bodyRotatePdf;
+    const blocks = document.body.querySelectorAll('.eventwrapper');
+    blocks.forEach((x) => init(x));
+    window.dispatchEvent(new CustomEvent('DC_Hosted:Ready'));
+    window.dc_hosted.dispatchEvent('preview-generating', {});
+    expect(document.body.dataset.currentEvent).to.eql('preview');
+    expect(document.body.classList.contains('l2-state')).to.be.equal(false);
+    expect(document.body.classList.contains('hide-content')).to.be.equal(false);
+    const widget = document.querySelector('[data-section="widget"]');
+    expect(widget.classList.contains('widget-default-height')).to.be.equal(true);
+    const contentSections = document.querySelectorAll('.section');
+    contentSections.forEach((section) => expect(section.classList.contains('hide')).to.be.equal(false));
+  });
+});
+
+describe('eventwrapper block on Microsoft Edge', () => {
+  let chromeRuntimeSendMessage = false;
+
+  before(() => {
+    window.browser = {
+      name: 'Microsoft Edge',
+      isMobile: false,
+    };
+    window.dc_hosted = {
+      events: [],
+      listeners: [],
+      addEventListener: function (fn) {
+        window.dc_hosted.listeners.push(fn);
+        window.dc_hosted.events.forEach(function (e) {
+          try {
+            fn(e.event, e.data);
+          } catch (ex) {
+            console.error('Error in addEventListener: ', ex, ex.stack);
+          }
+        });
+      },
+      dispatchEvent: function (event, data) {
+        window.dc_hosted.events.push({ event: event, data: data });
+        window.dc_hosted.listeners.forEach(function (fn) {
+          try {
+            fn(event, data);
+          } catch (e) {
+            console.error('Error in dispatchEvent: ', e, e.stack);
+          }
+        });
+      },
+    };
+    window.chrome = {
+      runtime: {
+        sendMessage: function (message, version, callback) {
+          callback((() => chromeRuntimeSendMessage)());
+        },
+      },
+    };
+    window._satellite = {
+      track: sinon.stub(),
+    };
+  });
+
+  afterEach(() => {
+    sinon.resetHistory();
+  });
+
+  after(() => {
+    sinon.restore();
+  });
+
+  it('shows the ext modal on MS Edge when conversion complete and preview displayed', async function () {
+    document.head.innerHTML = head;
+    document.body.innerHTML = body;
+    window.dc_hosted.listeners = [];
+    localStorage.removeItem('fricBrowExt');
+    window.modalDisplayed = false;
+    const blocks = document.body.querySelectorAll('.eventwrapper');
+    blocks.forEach((x) => init(x));
+    window.dispatchEvent(new CustomEvent('DC_Hosted:Ready'));
+    window.dc_hosted.dispatchEvent('conversion-complete', {});
+    expect(window.modalDisplayed).to.be.true;
+
+    window.modalDisplayed = false;
+    window.dc_hosted.dispatchEvent('preview-displayed', {});
+    expect(window.modalDisplayed).to.be.true;
+  });
+
+  it('handles modalExist', async function () {
+    document.head.innerHTML = head;
+    document.body.innerHTML = body;
+    window.dc_hosted.listeners = [];
+    localStorage.removeItem('fricBrowExt');
+    window.modalDisplayed = false;
+    chromeRuntimeSendMessage = true;
+    const blocks = document.body.querySelectorAll('.eventwrapper');
+    blocks.forEach((x) => init(x));
+    window.dispatchEvent(new CustomEvent('DC_Hosted:Ready'));
+    window.dc_hosted.dispatchEvent('conversion-complete', {});
+
+    window.modalDisplayed = false;
+    window.dc_hosted.dispatchEvent('preview-displayed', {});
+
+    // modalExist
+    const event = window._satellite.track.args[1][1].data._adobe_corpnew.digitalData.primaryEvent.eventInfo.eventName;
+    expect(event).to.eql('Get the extension-1|viewer-extension-exists|MSFT-Edge-extension');
+  });
+
+  it('handles modalAlready', async function () {
+    document.head.innerHTML = head;
+    document.body.innerHTML = body;
+    localStorage.fricBrowExt = 'true';
+    const blocks = document.body.querySelectorAll('.eventwrapper');
+    blocks.forEach((x) => init(x));
+    window.dispatchEvent(new CustomEvent('DC_Hosted:Ready'));
+
+    // modalAlready
+    const event = window._satellite.track.args[1][1].data._adobe_corpnew.digitalData.primaryEvent.eventInfo.eventName;
+    expect(event).to.eql('Get the extension-1|already-closed-viewer-extension|MSFT-Edge-extension');
   });
 });
---
+++ test/scripts/scripts-getLocale.jest.js
@@ -0,0 +1,35 @@
+/**
+ * @jest-environment jsdom
+ */
+
+describe('Test locale', () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
+  it('has langstore locale', async () => {
+    delete window.location;
+    window.location = new URL(
+      'https://www.adobe.com/langstore/ar/acrobat/online/ppt-to-pdf.html'
+    );
+    await require('../../acrobat/scripts/scripts');
+  });
+});
+
+describe('Test Navigator', () => {
+  beforeEach(() => {
+    jest.resetModules();
+    Object.defineProperty(global.navigator, 'userAgent', {
+      value: '',
+    });
+  });
+
+  it('has no navigator', async () => {
+    delete window.location;
+    window.location = new URL(
+      'https://www.adobe.com/acrobat/online/ppt-to-pdf.html'
+    );
+    await require('../../acrobat/scripts/scripts');
+    expect(window.browser).toStrictEqual({});
+  });
+});
