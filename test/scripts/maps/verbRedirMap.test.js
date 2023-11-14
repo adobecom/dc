eca6e2f1932bbadab2f2b9209ad008b015c25e9e
Dragana Trajkovic <dragana.trajkovic@lilly021.com>
2023-11-14 16:54:11 +0100

    eslint fix3

--- test/scripts/maps/verbRedirMap.test.js
+++ test/scripts/maps/verbRedirMap.test.js
@@ -7,7 +7,7 @@ describe('verb redirection map', () => {
     verbRedirMap = verbRedirMapImport.default;
   });
   it('should return valid redirection for known verbs', () => {
-    expect(verbRedirMap['createpdf']).to.be.equal('createpdf');
+    expect(verbRedirMap.createpdf).to.be.equal('createpdf');
     expect(verbRedirMap['crop-pages']).to.be.equal('crop');
     expect(verbRedirMap['delete-pages']).to.be.equal('deletepages');
     expect(verbRedirMap['extract-pages']).to.be.equal('extract');
@@ -16,9 +16,9 @@ describe('verb redirection map', () => {
     expect(verbRedirMap['add-comment']).to.be.equal('addcomment');
     expect(verbRedirMap['pdf-to-image']).to.be.equal('pdftoimage');
     expect(verbRedirMap['reorder-pages']).to.be.equal('reorderpages');
-    expect(verbRedirMap['sendforsignature']).to.be.equal('sendforsignature');
+    expect(verbRedirMap.sendforsignature).to.be.equal('sendforsignature');
     expect(verbRedirMap['rotate-pages']).to.be.equal('rotatepages');
-    expect(verbRedirMap['fillsign']).to.be.equal('fillsign');
+    expect(verbRedirMap.fillsign).to.be.equal('fillsign');
     expect(verbRedirMap['split-pdf']).to.be.equal('split');
     expect(verbRedirMap['insert-pdf']).to.be.equal('insert');
     expect(verbRedirMap['compress-pdf']).to.be.equal('compress');
