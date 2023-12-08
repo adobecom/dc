0c8af6d31cb71e4b25d2cd760a58b33c1c932663
James Tsay <tsay@adobe.com>
2023-12-08 13:11:07 -0800

    Fix for IE11

--- test/scripts/legacy.test.js
+++ test/scripts/legacy.test.js
@@ -1,16 +1,8 @@
 import { expect } from '@esm-bundle/chai';
-import { getBrowserData } from '../../acrobat/scripts/legacy.js';
 
 describe('legacy test', () => {
-  it('should return an empty object if userAgent is not provided', async () => {
-    const userAgent = null;
-    const browserData = getBrowserData(userAgent);
-    expect(browserData).to.be.an('object').that.is.empty;
-  });
-  it('should return browser data when userAgent is provided', async () => {
-    const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36\n';
-    const browserData = getBrowserData(userAgent);
-    expect(browserData).to.be.an('object');
-    expect(browserData).to.have.property('ua').that.equals(userAgent);
+  it('window.browser is assigned', async () => {
+    await import('../../../acrobat/scripts/legacy.js');
+    expect(window.browser).to.be.an('object');
   });
 });
