c8fa3e794051d839fe11f1e52cdc516f6c758395
James Tsay <tsay@adobe.com>
2024-01-18 11:45:07 -0800

    POC edge worker deployment

---
+++ edgeworkers/Acrobat_DC_web_stg/utils/verbs.js
@@ -0,0 +1,8 @@
+export default {
+  'add-pages-to-pdf': 'insert-pdf',
+  'compress-pdf': 'compress-pdf',
+  'extract-pdf-pages': 'extract-pages',
+  'pdf-to-ppt': 'pdf-to-ppt',
+  'merge-pdf': 'combine-pdf',
+  'rearrange-pdf': 'reorder-pages',
+};
