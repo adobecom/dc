9d1f95d56f9046a18d18b40433452f0a383551cf
Joaquin Rivero <jq.rivero@gmail.com>
2024-02-06 15:00:08 -0500

    no message

--- acrobat/scripts/fallback.js
+++ acrobat/scripts/fallback.js
@@ -4,6 +4,7 @@
     { pattern: /^\/acrobat\/online\/.*/, redirect: 'https://acrobat.adobe.com/home/index-browser-eol.html' },
     { pattern: /.*/, redirect: 'https://helpx.adobe.com/x-productkb/global/adobe-supported-browsers.html' }
   ];
+  alert('fallback.js')
 
   window.getRedirectURL = function(currentPath) {
     var route = routes.find(r => r.pattern.test(currentPath));
