acb491fe443cee73baf204834b4d1a5dfa85bd61
Joaquin Rivero <jq.rivero@gmail.com>
2024-02-07 07:52:52 -0500

    no message

--- acrobat/scripts/fallback.js
+++ acrobat/scripts/fallback.js
@@ -1,21 +1,18 @@
 /* eslint-disable */
-(function() {
-  var routes = [
-    { pattern: /^\/acrobat\/online\/.*/, redirect: 'https://acrobat.adobe.com/home/index-browser-eol.html' },
-    { pattern: /.*/, redirect: 'https://helpx.adobe.com/x-productkb/global/adobe-supported-browsers.html' }
-  ];
-  alert('fallback.js')
+var routes = [
+  { pattern: /^\/acrobat\/online\/.*/, redirect: 'https://acrobat.adobe.com/home/index-browser-eol.html' },
+  { pattern: /.*/, redirect: 'https://acrobat.adobe.com/home/index-browser-eol.html' }
+];
 
-  window.getRedirectURL = function(currentPath) {
-    var route = routes.find(r => r.pattern.test(currentPath));
-    return route ? route.redirect : null;
-  };
+var redirectToSupportPage = function() {
+  var currentPathname = window.location.pathname;
+  for (var i = 0; i < routes.length; i++) {
+    if (routes[i].pattern.test(currentPathname)) {
+      window.location.href = routes[i].redirect;
+      break;
+    }
+  }
+};
 
-  window.redirectToURL = function(url) {
-    window.location.href = url;
-  };
-
-  window.redirectToURL(window.getRedirectURL(window.location.pathname));
-
-})();
+redirectToSupportPage();
 /* eslint-enable */
