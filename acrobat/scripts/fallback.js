119a5c978d9b16096da5758b519aa4c1e47d3aed
Joaquin Rivero <jq.rivero@gmail.com>
2024-02-07 08:47:22 -0500

    no message

--- acrobat/scripts/fallback.js
+++ acrobat/scripts/fallback.js
@@ -1,18 +1,47 @@
 /* eslint-disable */
 var routes = [
-  { pattern: /^\/acrobat\/online\/.*/, redirect: 'https://acrobat.adobe.com/home/index-browser-eol.html' },
-  { pattern: /.*/, redirect: 'https://acrobat.adobe.com/home/index-browser-eol.html' }
+  {
+    pattern: /^\/([a-zA-Z]{2}(?:-[a-zA-Z]{2})?)\/acrobat\/online\/.*/,
+    redirect: function(matches) {
+      var locale = matches[1];
+      return 'https://acrobat.adobe.com/' + locale + '/home/index-browser-eol.html';
+    }
+  },
+  {
+    pattern: /^\/acrobat\/online\/.*/,
+    redirect: function() {
+      return 'https://acrobat.adobe.com/home/index-browser-eol.html';
+    }
+  },
+  {
+    pattern: /^\/([a-zA-Z]{2}(?:-[a-zA-Z]{2})?)\/(.*)/,
+    redirect: function(matches) {
+      var locale = matches[1];
+      var path = matches[2];
+      // Redirect to a general page with the locale
+      return 'https://helpx.adobe.com/' + locale + '/x-productkb/global/adobe-supported-browsers.html';
+    }
+  },
+  {
+    pattern: /.*/,
+    redirect: function() {
+      return 'https://helpx.adobe.com/x-productkb/global/adobe-supported-browsers.html';
+    }
+  }
 ];
 
-var redirectToSupportPage = function() {
+window.redirectToSupportPage = function() {
   var currentPathname = window.location.pathname;
+
   for (var i = 0; i < routes.length; i++) {
-    if (routes[i].pattern.test(currentPathname)) {
-      window.location.href = routes[i].redirect;
+    var match = currentPathname.match(routes[i].pattern);
+    if (match) {
+      var redirectUrl = routes[i].redirect(match);
+      window.location.href = redirectUrl;
       break;
     }
   }
 };
 
-redirectToSupportPage();
+window.redirectToSupportPage();
 /* eslint-enable */
