26ab9bb8a1410dd88341593c027fc603c00de93e
Blaine Gunn <blainegunn@Blaines-MacBook-Pro.local>
2024-02-16 12:19:23 -0700

    MWPW-142806

--- acrobat/scripts/scripts.js
+++ acrobat/scripts/scripts.js
@@ -283,7 +283,7 @@ if (window.location.pathname.match('/sign/')
   || window.location.pathname.match('/documentcloud/')
   || window.location.pathname.match('/acrobat/business/')) {
   CONFIG.jarvis.id = 'DocumentCloudsignAcro';
-};
+}
 
 // Default to loading the first image as eager.
 (async function loadLCPImage() {
