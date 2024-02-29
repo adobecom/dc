4245f8af30f83ffc9c3927b6a1a80ac1dab2636c
Blaine Gunn <blainegunn@blaines-mbp.lan>
2024-02-29 07:56:11 -0700

    MWPW-142826 for demo, not complete

---
+++ acrobat/scripts/geo-phoneNumber.js
@@ -0,0 +1,36 @@
+/* eslint-disable compat/compat */
+const urlParams = new URLSearchParams(window.location.search);
+let newLocale = urlParams.get('akamaiLocale') || JSON.parse(sessionStorage.getItem('feds_location')).country.toLowerCase() || '';
+if (newLocale === 'us') newLocale = '';
+if (newLocale !== 'us') newLocale = `${newLocale}/`;
+
+const replaceTypeOfNum = (numType, visNum, i) => {
+  const cc = document.querySelector(`.${i}`);
+  console.log(cc);
+  cc.querySelector('a').href = `tel:${visNum}`;
+  cc.querySelector('a').innerText = visNum;
+};
+
+// This funct
+export default async function fillerforPH() {
+  const response = await fetch(`/${newLocale}dc-shared/placeholders.json`);
+  const data = await response.text();
+  const DATA = JSON.parse(data);
+
+  document.querySelectorAll('p[class*="geo-pn"]').forEach((p) => {
+    const numberType = p.getAttribute('number-type');
+    const numberID = p.className;
+    DATA.data.forEach((val) => {
+      if (val.key === numberType) {
+        replaceTypeOfNum(numberType, val.value, numberID);
+      }
+    });
+  });
+}
+
+
+// Update Var names
+// Unit Test 
+// Test on real pages 
+// Test w/ hyperlink
+// Test when no geo is present
