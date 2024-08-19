e4dde7cf76e956fe04d60dca779c2de3c2671a55
Adam Peller <46139+peller@users.noreply.github.com>
2024-08-19 13:50:41 -0400

    add error detail to log

--- acrobat/blocks/dc-converter-widget/dc-converter-widget.js
+++ acrobat/blocks/dc-converter-widget/dc-converter-widget.js
@@ -337,7 +337,7 @@ export default async function init(element) {
     });
   });
 
-  window.addEventListener('DC_Hosted:Error', () => {
+  window.addEventListener('DC_Hosted:Error', (err) => {
     const dropZone = document.querySelector('.dropZoneContent');
     if (dropZone && !dropZone.classList.contains('unavailable')) {
       dropZone.classList.add('unavailable');
@@ -347,6 +347,6 @@ export default async function init(element) {
       dropZone.innerHTML = '<img src="/acrobat/img/icons/error.svg"><p>We apologize for the inconvenience. We are working hard to make the service available. Please check back shortly.</p>';
       document.querySelector('div[class*="DropZoneFooter__dropzoneFooter"]').innerHTML = '';
     }
-    window.lana?.log('DC Widget failed', lanaOptions);
+    window.lana?.log('DC Widget failed. detail: ${JSON.stringify(err.detail)}', lanaOptions);
   });
 }
