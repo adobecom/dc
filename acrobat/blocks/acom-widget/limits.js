03182a837ae1594cc2b9a8845ca1f5f0f8ca12f3
Blaine Gunn <Blainegunn@gmail.com>
2024-08-19 20:06:24 -0600

    MWPW-156303

---
+++ acrobat/blocks/acom-widget/limits.js
@@ -0,0 +1,24 @@
+const LIMITS = {
+  fillsign: {
+    maxFileSize: 100000000, // 1 MB
+    acceptedFiles: ['application/pdf'],
+    maxNumFiles: 1,
+  },
+  'delete-pages': {
+    maxFileSize: 100000000, // 1 MB
+    acceptedFiles: ['application/pdf'],
+    maxNumFiles: 1,
+  },
+  'number-pages': {
+    maxFileSize: 100000000, // 1 MB
+    acceptedFiles: ['application/pdf'],
+    maxNumFiles: 1,
+  },
+  'compress-pdf': {
+    maxFileSize: 100000000,
+    acceptedFiles: ['application/pdf'],
+    maxNumFiles: 1,
+  },
+};
+
+export default LIMITS;
