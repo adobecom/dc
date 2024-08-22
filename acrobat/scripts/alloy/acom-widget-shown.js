79807b3c4fa805ae13c3165eb039e594566578ca
saragajic11 <saragajic115@gmail.com>
2024-08-22 14:02:33 +0200

    Analytics for acom widget

---
+++ acrobat/scripts/alloy/acom-widget-shown.js
@@ -0,0 +1,57 @@
+const params = new Proxy(
+  // eslint-disable-next-line compat/compat
+  new URLSearchParams(window.location.search),
+  { get: (searchParams, prop) => searchParams.get(prop) },
+);
+
+let appReferrer = params.x_api_client_id || params['x-product'] || '';
+if (params.x_api_client_location || params['x-product-location']) {
+  appReferrer = `${appReferrer}:${params.x_api_client_location || params['x-product-location']}`;
+}
+let trackingId = params.trackingid || '';
+if (params.mv) {
+  trackingId = `${trackingId}:${params.mv}`;
+}
+if (params.mv2) {
+  trackingId = `${trackingId}:${params.mv2}`;
+}
+const appTags = [];
+if (params.workflow) {
+  appTags.push(params.workflow);
+}
+if (params.dropzone2) {
+  appTags.push('dropzone2');
+}
+
+export default function init(verb) {
+  const event = {
+    documentUnloading: true,
+    data: {
+      eventType: 'web.webinteraction.linkClicks',
+      web: {
+        webInteraction: {
+          linkClicks: { value: 1 },
+          type: 'other',
+          name: `acrobat:verb-${verb}:landing:shown`,
+        },
+      },
+      _adobe_corpnew: {
+        digitalData: {
+          dcweb: { event: { pagename: `acrobat:verb-${verb}:landing:shown` } },
+          dcweb2: {
+            event: { pagename: `acrobat:verb-${verb}:landing:shown` },
+          },
+        },
+      },
+    },
+  };
+  // Alloy Ready...
+  const AlloyReady = setInterval(() => {
+    // eslint-disable-next-line no-underscore-dangle
+    if (window?._satellite?.track) {
+      clearInterval(AlloyReady);
+      // eslint-disable-next-line no-underscore-dangle
+      window?._satellite?.track('event', event);
+    }
+  }, 1000);
+}
