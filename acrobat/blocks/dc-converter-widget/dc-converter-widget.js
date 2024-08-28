b2a0a976101b1bdb804d60b908f4e3c49b72817e
Blaine Gunn <Blainegunn@gmail.com>
2024-08-28 10:42:31 -0600

    Apply suggestions from code review
    lint fixes
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- acrobat/blocks/dc-converter-widget/dc-converter-widget.js
+++ acrobat/blocks/dc-converter-widget/dc-converter-widget.js
@@ -333,7 +333,7 @@ export default async function init(element) {
 
       window.dispatchEvent(personalizationIsReady);
     }).catch((err) => {
-      window.dispatchEvent(new CustomEvent('DC_Hosted:Error', { detail: { wrappedException: err }}));
+      window.dispatchEvent(new CustomEvent('DC_Hosted:Error', { detail: { wrappedException: err } }));
     });
   });
 
@@ -348,7 +348,7 @@ export default async function init(element) {
       document.querySelector('div[class*="DropZoneFooter__dropzoneFooter"]').innerHTML = '';
     }
     const { message, name, type } = err.detail?.wrappedException || {};
-    const info = `DC Widget failed. type=${type} name=${name} message=${message}`
+    const info = `DC Widget failed. type=${type} name=${name} message=${message}`;
     window.lana?.log(info, lanaOptions);
   });
 }
