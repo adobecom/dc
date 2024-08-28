dcd4de6100fcc18bd648824827d21837ed933ce8
Adam Peller <46139+peller@users.noreply.github.com>
2024-08-27 22:17:02 -0400

    Update acrobat/scripts/scripts.js
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- acrobat/scripts/scripts.js
+++ acrobat/scripts/scripts.js
@@ -466,7 +466,7 @@ const { ietf } = getLocale(locales);
     const imsIsReady = new CustomEvent('IMS:Ready');
     window.dispatchEvent(imsIsReady);
   }).catch((err) => {
-    window.dispatchEvent(new CustomEvent('DC_Hosted:Error', { detail: { wrappedException: err }}));
+    window.dispatchEvent(new CustomEvent('DC_Hosted:Error', { detail: { wrappedException: err } }));
   });
 
   loadLana({ clientId: 'dxdc', tags: 'DC_Milo' });
