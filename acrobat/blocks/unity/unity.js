54a17b2df13651336cc6a29f88647f6664c7837a
saragajic11 <saragajic115@gmail.com>
2024-09-04 08:15:47 +0200

    implement integration with project unity

---
+++ acrobat/blocks/unity/unity.js
@@ -0,0 +1,23 @@
+import { loadStyle } from '../../scripts/utils.js';
+
+function getUnityLibs(prodLibs = '/unitylibs') {
+  const { hostname } = window.location;
+  if (!hostname.includes('hlx.page')
+    && !hostname.includes('hlx.live')
+    && !hostname.includes('localhost')) {
+    return prodLibs;
+  }
+  const branch = new URLSearchParams(window.location.search).get('unitylibs') || 'main';
+  if (branch.indexOf('--') > -1) return `https://${branch}.hlx.live/unitylibs`;
+  return `https://${branch}--unity--adobecom.hlx.live/unitylibs`;
+}
+
+export default async function init(el) {
+  const unitylibs = getUnityLibs();
+  const stylePromise = new Promise((resolve) => {
+    loadStyle(`${unitylibs}/core/styles/styles.css`, resolve);
+  });
+  await stylePromise;
+  const { default: wfinit } = await import(`${unitylibs}/core/workflow/workflow.js`);
+  await wfinit(el, 'acrobat', unitylibs);
+}
