b673c9b0d38b964f993e59a2d798f0470921368c
saragajic11 <saragajic115@gmail.com>
2024-09-24 11:05:15 +0200

    implemented analytics

--- acrobat/scripts/susiAuthHandler.js
+++ acrobat/scripts/susiAuthHandler.js
@@ -1,13 +1,17 @@
+import susiAnalytics from '../scripts/alloy/susi-auth-handler.js';
+
 export default function handleImsSusi(susiElems) {
   susiElems.forEach((link) => {
     if (link.href.includes('-sign-up')) {
       link.addEventListener('click', (e) => {
         e.preventDefault();
+        susiAnalytics(link.text, 'sign-up', window.browser.isMobile ? 'mobile' : 'desktop');
         window.adobeIMS.signUp();
       });
     } else if (link.href.includes('-sign-in')) {
       link.addEventListener('click', (e) => {
         e.preventDefault();
+        susiAnalytics(link.text, 'sign-in', window.browser.isMobile ? 'mobile' : 'desktop');
         window.adobeIMS.signIn();
       });
     }
