50bfee8ede4deecd314f914ccec5e12773ccb703
James Tsay <tsay@adobe.com>
2024-09-27 14:40:20 -0700

    Fix test

--- test/scripts/scripts-preloads.test.js
+++ test/scripts/scripts-preloads.test.js
@@ -1,10 +1,12 @@
 import { expect } from '@esm-bundle/chai';
+import { waitForElement } from '../helpers/waitfor.js';
 
 describe('Test scripts.js configurable preloads', () => {
   it('has preload metadata', async () => {
     document.head.innerHTML = '<link rel="icon" href="data:,"><meta name="preloads" content="$MILOLIBS/blocks/marquee/marquee.js,$MILOLIBS/blocks/marquee/marquee.css,https://www.example.com/abc.js"/>';
     document.body.innerHTML = '<main><div class="marquee"></main>';
     await import('../../acrobat/scripts/scripts.js');
+    await waitForElement('link[rel="stylesheet"][href="/acrobat/styles/styles.css"]');
     const js = document.querySelector('link[rel="preload"][href="https://main--milo--adobecom.hlx.page/libs/blocks/marquee/marquee.js"]');
     const css = document.querySelector('link[rel="stylesheet"][href="https://main--milo--adobecom.hlx.page/libs/blocks/marquee/marquee.css"]');
     const other = document.querySelector('link[rel="preload"][href="https://www.example.com/abc.js"]');
