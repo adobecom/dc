e72e6d469db7bcd2c490d396627a63d4579c5fc0
Joaquin Rivero <jq.rivero@gmail.com>
2024-11-22 12:28:10 +0800

    Adding tracking to cancel button

--- acrobat/blocks/verb-widget/verb-widget.js
+++ acrobat/blocks/verb-widget/verb-widget.js
@@ -252,6 +252,11 @@ export default async function init(element) {
       setUser();
     }
 
+    if (e.detail?.event === 'cancel') {
+      verbAnalytics('job:cancel', VERB, e.detail?.data);
+      setUser();
+    }
+
     if (e.detail?.event === 'uploading') {
       verbAnalytics('job:uploading', VERB, e.detail?.data);
       setUser();
