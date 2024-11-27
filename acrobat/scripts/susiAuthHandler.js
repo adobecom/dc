b10fd510f604d4a2b8459e2f0c0f74952791e42b
Joaquin Rivero <jq.rivero@gmail.com>
2024-11-27 09:58:55 +0800

    Transform link to button to avoid link behaviors

--- acrobat/scripts/susiAuthHandler.js
+++ acrobat/scripts/susiAuthHandler.js
@@ -1,21 +1,30 @@
+import { setLibs } from './utils.js';
 import susiAnalytics from './alloy/susi-auth-handler.js';
+
+const miloLibs = setLibs('/libs');
+const { createTag } = await import(`${miloLibs}/utils/utils.js`);
+
+async function handleButton(link, action) {
+  const originalClasses = Array.from(link.classList);
+  const button = createTag('button', { class: `susi-cta ${originalClasses.join(' ')}` }, link.text);
+  link.replaceWith(button);
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+    susiAnalytics(link.text, `${action}`, window.browser.isMobile ? 'mobile' : 'desktop');
+    if (action === 'sign-up') {
+      window.adobeIMS.signUp();
+    } else if (action === 'sign-in') {
+      window.adobeIMS.signIn();
+    }
+  });
+}
 
 export default function handleImsSusi(susiElems) {
   susiElems.forEach((link) => {
     if (link.href.includes('-sign-up')) {
-      link.addEventListener('click', (e) => {
-        e.preventDefault();
-        susiAnalytics(link.text, 'sign-up', window.browser.isMobile ? 'mobile' : 'desktop');
-        window.adobeIMS.signUp();
-      });
+      handleButton(link, 'sign-up');
     } else if (link.href.includes('-sign-in')) {
-      link.addEventListener('click', (e) => {
-        e.preventDefault();
-        susiAnalytics(link.text, 'sign-in', window.browser.isMobile ? 'mobile' : 'desktop');
-        window.adobeIMS.signIn();
-      });
+      handleButton(link, 'sign-in');
     }
-    link.style.pointerEvents = 'auto';
-    link.style.cursor = 'pointer';
   });
 }
