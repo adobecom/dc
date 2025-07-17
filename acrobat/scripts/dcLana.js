3ab508e8f684b827657969a5b7832f2689eb13d6
Blaine Gunn <Blainegunn@gmail.com>
2025-07-17 11:36:43 -0600

    MWPW-175724

--- acrobat/scripts/dcLana.js
+++ acrobat/scripts/dcLana.js
@@ -4,24 +4,6 @@ export default function lanaLogging() {
 
   const lanaCspOptions = { sampleRate: 0.001 };
 
-  // DC Converter Logging
-  window.addEventListener('DC_Hosted:Ready', () => {
-    const LANA_DC_WIDGET = document.querySelectorAll('[class*=ErrorDisplay]');
-    LANA_DC_WIDGET.forEach((lanaDcw) => {
-      if (fricPage && lanaDcw.querySelector('[class*=ErrorDisplay]')) {
-        lanaOptions.tags = 'DC_Milo,Frictionless';
-        window.lana?.log('DC Widget Failed ¶ Reason: Error', lanaOptions);
-      }
-    });
-  });
-
-  setTimeout(() => {
-    if (fricPage && !window.dc_hosted) {
-      lanaOptions.tags = 'DC_Milo,Frictionless';
-      window.lana?.log('DC Widget Didn\'t Load ¶ Reason: DC Hosted did not load', lanaOptions);
-    }
-  }, 10000);
-
   window.dcwErrors?.forEach((error) => {
     lanaOptions.tags = 'DC_Milo,Frictionless';
     window.lana?.log(error, lanaOptions);
