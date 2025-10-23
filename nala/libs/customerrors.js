e9ab8e6c1716d01bb22b5507be1fd8a1994e0cda
Ruchika Sinha <69535463+Ruchika4@users.noreply.github.com>
2025-10-23 11:45:47 -0700

    Revert "[MWPW-181214][NALA] Onboard Nala Framework for Automated Testing in D…" (#1308)
    This reverts commit 8f2121d40039e9c141da0201a7e5b148ab2e3efc.

--- nala/libs/customerrors.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-
-export class AccessibilityError extends Error {
-  constructor(messages) {
-    super(messages);
-    this.name = 'AccessibilityError';
-  }
-}
